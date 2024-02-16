9a84de6d10ab1a7d6bb050f5c46fdad4afdc2799
Muhammad Mujtaba Tahir <97501928+Muji47@users.noreply.github.com>
2024-02-16 19:26:26 +0500

    Add files via upload

---
+++ CurrConverter.js
@@ -0,0 +1,42 @@
+
+const handleChangeAmount=()=>{
+  let currAmount=document.getElementById("amountcon")
+  let amountTo=document.getElementById("curr_to").value
+  let amount=document.getElementById("amount")
+  amount.innerText=currAmount.value*amountTo
+  currAmount.innerText="";
+}
+async function fetchDataWithErrors() {
+    try {
+      const response = await fetch('https://openexchangerates.org/api/latest.json?app_id=cd24bc83a444456e9966b810fc9ce7ca');
+      if (!response.ok) {
+        throw new Error('Request failed with status ' + response.status);
+      }
+      const data = await response.json();
+      console.log(data)
+      var mySelectFrom = document.getElementById("curr_from");
+      var mySelectTo = document.getElementById("curr_to");
+
+for (let x in data.rates) {
+  var opt = document.createElement('option');
+        opt.value = data.rates[x];
+        opt.innerHTML = x;
+        mySelectFrom.appendChild(opt);
+}
+for (let x in data.rates) {
+  var opt = document.createElement('option');
+        opt.value = data.rates[x];
+        opt.innerHTML = x;
+        mySelectTo.appendChild(opt);
+}
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  }
+  
+  fetchDataWithErrors();
+  
+// var jsonData;
+// fetch('https://openexchangerates.org/api/latest.json?app_id=cd24bc83a444456e9966b810fc9ce7ca').then(response=> response.json()).then(data=>{jsonData=data;
+// });
+// console.log(jsonData);
